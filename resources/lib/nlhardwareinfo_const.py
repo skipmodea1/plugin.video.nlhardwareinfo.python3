da50bad45c90936af5b59ff7546730579b0e3c4c
SkipmodeA1 <skipmodea1@outlook.com>
2026-05-11 16:51:25 +0200

    v1.0.9 (2026-05-11)
    - marked addon BROKEN because website is discontinued

--- resources/lib/nlhardwareinfo_const.py
+++ resources/lib/nlhardwareinfo_const.py
@@ -15,7 +15,7 @@ SETTINGS = xbmcaddon.Addon()
 LANGUAGE = SETTINGS.getLocalizedString
 IMAGES_PATH = os.path.join(xbmcaddon.Addon().getAddonInfo('path'), 'resources')
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-DATE = "2026-05-03"
+DATE = "2026-05-11"
 VERSION = "1.0.9"
 
 
